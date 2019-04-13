9b74a0ab973678034950155237f5d4e6a093e374
mclopes1 <cruzlopesmurilo@gmail.com>
2019-04-13 18:56:19 -0300

    criacao da classe que retorna os arquivos de entrada presente no diretorio teste

---
+++ Problema-01/src/lexico/FilesListed.java
@@ -0,0 +1,33 @@
+package lexico;
+
+import java.io.File;
+import java.util.LinkedList;
+
+public class FilesListed {
+	
+	private String directory_name;
+	
+	public FilesListed(String directory_name) {
+		this.directory_name = directory_name;
+	}
+	
+	public LinkedList<String> arquivosEntrada() {
+		String[] filesNames;
+		LinkedList<String> entry_files = new LinkedList<String>();
+		int tam;
+
+		File f = new File(directory_name);
+		if (f.isDirectory() == false)
+			return null;
+		
+		filesNames = f.list();
+		tam = filesNames.length;
+		for (int i = 0; i < tam; i++)
+			if (filesNames[i].contains(".entrada"))
+				entry_files.add(filesNames[i]);
+
+		return entry_files;
+	}
+
+	
+}
--- Problema-01/src/lexico/MainTeste.java
+++ Problema-01/src/lexico/MainTeste.java
@@ -2,9 +2,13 @@ package lexico;
 
 
 import java.io.IOException;
+import java.util.LinkedList;
 
 public class MainTeste {
 	public static void main(String[] args) {
+		//Essas duas linha abaixo acessa a pasta teste e retorna os nomes do arquivos com a extensao .entrada
+		LinkedList<String> files = new FilesListed("teste").arquivosEntrada();
+		
 		try {
            testarArquivo("teste.entrada");
 
